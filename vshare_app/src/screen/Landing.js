84090623689834452a342917c9660148d30a73a0
kamyab <amirhosseinghassemi78@yahoo.com>
2020-05-06 10:11:37 +0430

    Background image

--- vshare_app/src/screen/Landing.js
+++ vshare_app/src/screen/Landing.js
@@ -5,7 +5,23 @@ class Landing extends Component{
 render(){
     return(
 <View style={styles.container}>
-
+<ImageBackground style={styles.backgroundPhoto} source={require('./photo/landing.jpg')}>
+    {/* <View style={styles.viewHead}>
+        <View style={styles.ViewName}>
+            <Text>
+Vshare
+            </Text>
+        </View>
+        <View style={styles.ViewBtn}>
+              <TouchableOpacity
+                      style={styles.btnSign}
+                    //   onPress={() => this.props.navigation.navigate('History')}
+                    >
+                        <Text>login/signup</Text>
+                    </TouchableOpacity>>
+        </View>
+    </View> */}
+</ImageBackground>
 </View>
     )
 
@@ -18,6 +34,28 @@ const styles = StyleSheet.create({
         height:height,
         backgroundColor:'#31100f'
     },
+    backgroundPhoto:{
+           flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center"
+    },
+    ViewName:{
+justifyContent: 'flex-start'
+    },
+    textName:{
+        
+    },
+    ViewBtn:{
+ justifyContent: 'flex-end'
+    },
+    btnSign:{
+
+    },
+    viewHead:{
+width:'100%' ,
+ height:'10%',
+ flexDirection: 'row',
+    }
 
 })
 export default Landing;
