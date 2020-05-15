acdbd1e5a9a2143244a92b3c5b516865c1155c04
kamyab <amirhosseinghassemi78@yahoo.com>
2020-05-15 12:39:39 +0430

    Add stepper and show input

--- vshare_app/src/screen/Creategroup.js
+++ vshare_app/src/screen/Creategroup.js
@@ -1,14 +1,48 @@
 import React , {Component} from 'react';
 import{View ,StyleSheet,Dimensions , TextInput,Text,TouchableOpacity , StatusBar , Button } from 'react-native'
+import { ProgressSteps, ProgressStep } from 'react-native-progress-steps';;
 var{height , width}=Dimensions.get('window');
-class Create extends Component{
-    render(){
-        return(
-            <View style={styles.container}>
 
+const buttonTextStyle = {
+    color: 'red'
+};
+const prev={
+    color:'#121212'
+}
+class Create extends Component{
+
+
+render() {
+  return (
+  <View style={styles.container}>
+  <View style={{flex: 1}}>
+    <ProgressSteps>
+        <ProgressStep label="create group" nextBtnTextStyle={buttonTextStyle}>
+            <View style={{ alignItems: 'center' }}>
+                 <TextInput style={styles.inp1} placeholder='password'
+                placeholderTextColor='white'
+                ></TextInput>
+                  <TextInput style={styles.inp1} placeholder='password'
+                placeholderTextColor='white'
+                ></TextInput>
+                  <TextInput style={styles.inp1} placeholder='password'
+                placeholderTextColor='white'
+                ></TextInput>
             </View>
-        )
-    }
+        </ProgressStep>
+        <ProgressStep label="add member" nextBtnTextStyle={buttonTextStyle} previousBtnTextStyle={prev}>
+            <View style={{ alignItems: 'center' }}>
+                <Text>This is the content within step 2!</Text>
+            </View>
+        </ProgressStep>
+      
+    </ProgressSteps>
+</View>
+</View>
+  )
+}
+
+
 }
 const styles = StyleSheet.create({
     container:{
@@ -19,6 +53,15 @@ const styles = StyleSheet.create({
         justifyContent:'center',
        
     },
+    inp1:{
+           backgroundColor:'rgba(255,255,255,0.2)',
+        width:width*2/3,
+        borderRadius:25,
+        paddingHorizontal:16,
+        fontSize:16,
+        marginVertical:13,
+        color:'white'
+    }
   
 
 
